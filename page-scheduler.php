20f4b9b323084ae23ad0fd26e81f1712d23d046f
Marcio Fao <marcio.lopes.fao@gmail.com>
2024-04-29 22:09:28 -0300

    🗑️

--- page-scheduler.php
+++ page-scheduler.php
@@ -1,40 +1,29 @@
-
 <?php global $post; ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php wp_head() ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo get_the_title() ?></title>
-    <?php wp_head() ?>
-    <link rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ) ?>css/bootstrap.css">
-</head>
-<body>
-    
-    <div class="container">
-        <div class="row">
-            <h1><?php echo get_the_title() ?></h1>
-            <?php the_content() ?>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <?php if(!isset($_GET['appointment-saved'])): ?>
+<div class="container">
+    <div class="row">
+        <h1><?php echo get_the_title() ?></h1>
+        <?php the_content() ?>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <?php if (!isset($_GET['appointment-saved'])) : ?>
                 <form method="post" class="form-control" id="appointmentForm">
                     <div class="form-group">
                         <div class="row">
                             <div class="col-md-6 col-sm-12">
-                                <label for="date">Date</label>
+                                <label for="ms_date">Date</label>
                                 <input type="date" name="ms_date" id="date" class="form-control" required>
                             </div>
                             <div class="col-md-6 col-sm-12">
-                                <label for="time">Time</label>
+                                <label for="ms_time">Time</label>
                                 <input type="time" name="ms_time" id="time" class="form-control" required>
                             </div>
                         </div>
-                        <label for="name">Mentee's Name</label>
+                        <label for="ms_name">Mentee's Name</label>
                         <input type="text" name="ms_name" id="name" class="form-control" required>
-                        <label for="notes">Notes</label>
+                        <label for="ms_notes">Notes</label>
                         <textarea name="ms_notes" id="notes" cols="30" rows="10" class="form-control" required></textarea>
                         <input type="hidden" name="menthoring-scheduler" value="1">
 
@@ -42,17 +31,15 @@
 
                     </div>
                 </form>
-                <?php else: ?>
-                    <h2 class="text-center">Appointment Saved Sucessfully!</h2>
-                    <div class="text-center">
-                        <a href="<?php echo site_url().'/'.$post->post_name ?>">Add new appointment</a>
-                    </div>
-                <?php endif ?>
-            </div>
+            <?php else : ?>
+                <h2 class="text-center">Appointment Saved Sucessfully!</h2>
+                <div class="text-center">
+                    <a href="<?php echo site_url() . '/' . $post->post_name ?>">Add new appointment</a>
+                </div>
+            <?php endif ?>
         </div>
-        <script src="<?php echo plugin_dir_url( __FILE__ ) ?>js/validator.js"></script>
+    </div>
+</div>
+<script src="<?php echo plugin_dir_url(__FILE__) ?>js/validator.js"></script>
 
-        <?php wp_footer() ?>
-    </body>
-
-</html>
+<?php wp_footer() ?>
